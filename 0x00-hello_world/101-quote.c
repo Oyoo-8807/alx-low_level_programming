40b1e329a40c4357f8b94ad6ed79eab51acd1beb
Oyoo-8807 <maxweloyoo88@gmail.com>
2023-02-19 06:08:29 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * main - code the C language
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+       write(2, " the C language\" - Dora Korpar, 2015-10-19\n", 59);
+       return (1);
+
+}
