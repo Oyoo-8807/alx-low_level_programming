a2ee124d79780b47d9adc6b391568daadf7be6db
Oyoo-8807 <maxweloyoo88@gmail.com>
2023-02-19 04:48:46 -0800

    put 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - C programming language 
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}	
