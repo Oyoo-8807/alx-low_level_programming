a18d6ff3c4d3a5f9a03b1123864ee83d63bc4a39
Oyoo-8807 <maxweloyoo88@gmail.com>
2023-03-02 10:51:00 -0800

    wall

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,9 @@
  *
  * Return: A pointer to the string dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
+
 {
         int index = 0, src_len = 0;
 
@@ -18,7 +20,7 @@ char *_strncpy(char *dest, char *src, int n)
         for (index = 0; src[index] && index < n; index++)
                 dest[index] = src[index];
 
-	for (index = 0; src_len; index < n; index++)
+	for (index = 0; src_len; index++)
                 dest[index] = '\0';
 
         return (dest);
