bdb54c0c72ae258bbae3651ab4a2cea088186a0c
Oyoo-8807 <maxweloyoo88@gmail.com>
2023-03-02 13:26:06 -0800

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,12 +2,11 @@
 
 /**
  * cap_string - Capitalizes all words of a string
- * @str: string to be capitalized 
- * 
+ * @str: The string to be capitalised 
  * Return: pointer to the changed string
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int index = 0;
 
@@ -16,24 +15,23 @@ char *cap_string(char *)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index-1] == ' ' ||
-			str[index-1] == ' ' ||
-			str[index-1] == '\t' ||
-			str[index-1] == '\n' ||
-			str[index-1] == ',' ||
-			str[index-1] == ';' ||
-			str[index-1] == '.' ||
-			str[index-1] == '!' ||
-			str[index-1] == '?' ||
-			str[index-1] == '-' ||
-			str[index-1] == '(' ||
-			str[index-1] == ')' ||
-			str[index-1] == '}' ||
-			str[index-1] == '{' ||
-			str[index-1] == '' ||
-			[index-1] == 0)
-				str[index] -= 32;
-			index++;
+		if  (str[index - 1] == ' ' ||
+		     str[index - 1] == '\t' ||
+		     str[index - 1] == '\n' ||
+		     str[index - 1] == ',' ||
+		     str[index - 1] == ';' ||
+		     str[index - 1] == '.' ||
+		     str[index - 1] == '!' ||
+	             str[index - 1] == '?' ||
+		     str[index - 1] == '-' ||
+		     str[index - 1] == '(' ||
+		     str[index - 1] == ')' ||
+                     str[index - 1] == '{' ||
+                     str[index - 1] == '}' ||
+		     index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
 
 	return (str);
