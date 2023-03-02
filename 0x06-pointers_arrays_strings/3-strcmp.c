52b359a5e9e7abbde860c1fe421b3346a1de99c0
Oyoo-8807 <maxweloyoo88@gmail.com>
2023-03-02 11:29:05 -0800

    stamp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,13 +5,13 @@
  * @s2: Pointer for the second string be compared
  *
  * Return: If str1 < str2, the negative difference of the first matched char
- * 	If strl == str2, 0.
- * 	If str1 > str2, the positive difference of the first unmatched char
+ *   		If strl == str2, 0.
+ * 	 	If str1 > str2, the positive difference of the first unmatched char
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
